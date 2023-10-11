c3343a0a88457d5d7bed98d23829c75a037b3dc4
Ostap Ivakh <o.ivakh95@gmail.com>
2023-10-11 21:06:18 +0300

    Add newspaper-agency-geiu.onrender.com to ALLOWED_HOSTS

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-geiu.onrender.com"]
 
 
 INSTALLED_APPS = [

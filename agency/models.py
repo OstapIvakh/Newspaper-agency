0a61219da1cc3956802bf9bb968be98aa8f42f11
Ostap Ivakh <o.ivakh95@gmail.com>
2023-10-20 18:48:08 +0300

    Removed redundant verbose_name & verbose_name_plural

--- agency/models.py
+++ agency/models.py
@@ -6,10 +6,6 @@ from django.urls import reverse
 class Redactor(AbstractUser):
     years_of_experience = models.PositiveIntegerField(null=True, blank=True)
 
-    class Meta:
-        verbose_name = "redactor"
-        verbose_name_plural = "redactors"
-
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
